58dbb33f9c9216fe64a4fddfa68a4f26aa1418cf
Brian Suda <brian@suda.co.uk>
2014-12-07 22:47:10 +0000

    Updating the files and getting the database setting-up

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask
+from flask import request
+import os
+import datetime
+import json
+from smtplib import SMTP
+from cStringIO import StringIO
+from email.mime.multipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.header import Header
+from email import Charset
+from email.generator import Generator
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+class Member(db.Model):
+    id    = db.Column(db.Integer, primary_key=True)
+    name  = db.Column(db.String())
+    email = db.Column(db.String())
+
+    def __init__(self, name, email):
+        self.name  = name
+        self.email = email
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Message(db.Model):
+    id       = db.Column(db.Integer, primary_key=True)
+    email    = db.Column(db.String())
+    message  = db.Column(db.String())
+    date_did = db.Column(db.String())
+
+    def __init__(self, email, message, date_did):
+        self.message  = message
+        self.email    = email
+        self.date_did = date_did
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+def send_email(to_email,subject,message):
+    # send the message
+    smtp = SMTP()
+    smtp.connect('smtp.mandrillapp.com', 587)
+    smtp.login(os.environ.get('MANDRILL_USERNAME'), os.environ.get('MANDRILL_APIKEY'))
+    
+    from_addr = "Tindfell <whatididtoday@tindfell.com>"
+    to_addr = [to_email]
+    
+    date = datetime.datetime.now().strftime( "%d/%m/%Y %H:%M" )
+    
+    Charset.add_charset('utf-8', Charset.QP, Charset.QP, 'utf-8')
+    msg = MIMEMultipart("alternative")
+    
+    msg['From'] = Header(from_addr.encode('utf-8'), 'UTF-8').encode()
+    msg['To'] = Header(', '.join(to_addr).encode('utf-8'), 'UTF-8').encode()
+    msg['Subject'] = Header(subject.encode('utf-8'), 'UTF-8').encode()
+    
+    msg.attach(MIMEText(message.encode('utf-8'),'plain','utf-8'))
+    #msg.attach(MIMEText(message.encode('utf-8'),'html','utf-8'))
+    
+    io = StringIO()
+    g = Generator(io, False) # second argument means "should I mangle From?"
+    g.flatten(msg)
+    
+    # For Degubbing
+    #print io.getvalue()
+    
+    # send the message!
+    smtp.sendmail(from_addr, to_addr, io.getvalue())
+    smtp.quit()
+    return
+
+@app.route("/webhook", methods=['GET'])
+def webhook():
+	# Get the data from mandrill and save it into the database
+	
+	return "Error"
+
+
+@app.route("/ask", methods=['GET'])
+def ask():
+	# Here we look in the database, loop through users and send them an email asking what they have done today	
+	if 'api_key' in request.args:
+		if request.args['api_key'] == os.environ.get('API_KEY'):
+			# Now loop through the DB and send the standard email
+			subject = "What did you do today %s"%(datetime.datetime.today().strftime("%Y-%m-%d"))
+			members = db.session.query(Member).all()
+			for i in members:
+				# Send an email to each person, extra X-Headers if needed
+				send_email("%s <%s>"%(i.name,i.email),subject,"")
+			
+			return "Success"
+			
+	# If you are getting here, something failed	
+	return "Error"
+
+@app.route("/")
+def hello():
+    return "Welcome to Flask!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
